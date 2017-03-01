1ad36d6df6f158d8b138e12e9d713e592b74ba92
Bruno Santos <bhenrique.machado@gmail.com>
2017-02-28 21:12:37 -0300

    add login layout

---
+++ FriendlyDonation/FriendlyDonation/Controllers/LoginTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginTableViewController.swift
+//  FriendlyDonation
+//
+//  Created by Bruno Santos on 28/02/17.
+//  Copyright © 2017 Bruno Santos. All rights reserved.
+//
+
+import UIKit
+
+class LoginTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
